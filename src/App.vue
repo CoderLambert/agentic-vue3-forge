98661934a11074b2cf8d875533f43f5e123b0b77
风雨火 <l156486648@163.com>
2026-03-13 17:14:55 +0800

    feat: 添加 tailwindV4

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <h1 class="text-3xl font-bold text-sky-500 underline">Hello world!</h1>
+
+  <button class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Submit</button>
 </template>
 
 <style scoped></style>
